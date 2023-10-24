99b303746d918c99276cf7bc78a87d3138d86cda
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-10-23 21:16:08 -0700

    signup now working

--- src/Components/Auth/context.jsx
+++ src/Components/Auth/context.jsx
@@ -3,32 +3,6 @@ import cookie from "react-cookies";
 import jwt_decode from "jwt-decode";
 import axios from "axios";
 
-// const testUsers = {
-//   Administrator: {
-//     password: "admin",
-//     name: "Administrator",
-//     token:
-//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW5pc3RyYXRvciIsInJvbGUiOiJhZG1pbiIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJywncmVhZCcsJ3VwZGF0ZScsJ2RlbGV0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.pAZXAlTmC8fPELk2xHEaP1mUhR8egg9TH5rCyqZhZkQ",
-//   },
-//   Editor: {
-//     password: "editor",
-//     name: "Editor",
-//     token:
-//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRWRpdG9yIiwicm9sZSI6ImVkaXRvciIsImNhcGFiaWxpdGllcyI6IlsncmVhZCcsJ3VwZGF0ZSddIiwiaWF0IjoxNTE2MjM5MDIyfQ.3aDn3e2pf_J_1rZig8wj9RiT47Ae2Lw-AM-Nw4Tmy_s",
-//   },
-//   Writer: {
-//     password: "writer",
-//     name: "Writer",
-//     token:
-//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiV3JpdGVyIiwicm9sZSI6IndyaXRlciIsImNhcGFiaWxpdGllcyI6IlsnY3JlYXRlJ10iLCJpYXQiOjE1MTYyMzkwMjJ9.dmKh8m18mgQCCJp2xoh73HSOWprdwID32hZsXogLZ68",
-//   },
-//   User: {
-//     password: "user",
-//     name: "User",
-//     token:
-//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVXNlciIsInJvbGUiOiJ1c2VyIiwiY2FwYWJpbGl0aWVzIjoiWydyZWFkJ10iLCJpYXQiOjE1MTYyMzkwMjJ9.WXYvIKLdPz_Mm0XDYSOJo298ftuBqqjTzbRvCpxa9Go",
-//   },
-// };
 const capabilities = {
   'Administrator': ['create', 'update', 'delete'],
   'Editor': ['create', 'udpate'],
@@ -95,6 +69,24 @@ const LoginProvider = (props) => {
     }
   };
 
+  // const signup = async (username, password) => {
+  //   const config = {
+  //     baseURL:'https://bearer-auth-lab34.onrender.com',
+  //     url: '/signup',
+  //     method: 'post',
+  //     auth: { username, password }
+  //   }
+  //   const response = await axios(config);
+  //   console.log(response);
+
+    // const newUser = { username, capabilities: capabilities['User']};
+
+    // setState((prevState) => ({
+    //   ...prevState,
+    //   user: newUser,
+    // }));
+  // }
+
   useEffect(() => {
     if (state.user.name) return;
     const qs = new URLSearchParams(window.location.search);
@@ -103,7 +95,6 @@ const LoginProvider = (props) => {
     validateToken(token);
   }, [state.user.name, validateToken]);
 
-  
 
   return (
     <LoginContext.Provider value={{ ...state, can: can, login: login, logout: logout }}>
